0129a5fd5774113289d93baf8af31d5849fbcd25
kozr <zxzflow@gmail.com>
2023-10-14 14:05:08 -0700

    linting

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
+import logo from "./logo.svg";
 import "./App.css";
 import openai from "./utilities/openai";
-import { useState } from "react";
 
 function App() {
-	const [youtubeID] = useState("jeKFPHa9Kec?si=fAHuxJi46pqYoX4x");
+  const test = async () => {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: "Say this is a test" }],
+      model: "gpt-3.5-turbo",
+    });
 
-	const test = async () => {
-		const chatCompletion = await openai.chat.completions.create({
-			messages: [{ role: "user", content: "Say this is a test" }],
-			model: "gpt-3.5-turbo",
-		});
+    alert(chatCompletion.choices);
+  };
 
-		alert(chatCompletion.choices);
-	};
-	return (
-		<>
-			<button onClick={test}>Test OpenAI</button>
-			<div className="App">
-				<h1>Youtube embed: </h1>
-				<div className="youtube-container">
-					<iframe
-						className="video"
-						title="Youtube player"
-						sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
-						src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}
-					></iframe>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={test}>Test OpenAI</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
