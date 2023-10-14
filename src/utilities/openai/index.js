66fe7313a847abc40d758c392d75c5de7191a347
kozr <zxzflow@gmail.com>
2023-10-14 14:10:38 -0700

    revert accident

--- src/utilities/openai/index.js
+++ src/utilities/openai/index.js
@@ -1,8 +1,8 @@
-import OpenAI from 'openai';
+import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env['REACT_APP_OPENAI_API_KEY'],
-  dangerouslyAllowBrowser: true // It's fine for hackathon, but don't deploy
+  apiKey: process.env["REACT_APP_OPENAI_API_KEY"],
+  dangerouslyAllowBrowser: true, // It's fine for hackathon, but don't deploy
 });
 
 export default openai;
