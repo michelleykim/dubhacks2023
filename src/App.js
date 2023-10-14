045187c4a8df78d833f1b7fc031e53bee380fa1b
michelleykim <yeojin011016@gmail.com>
2023-10-14 14:01:39 -0700

    video embed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
-import openai from './utilities/openai';
+import openai from "./utilities/openai";
+import { useState } from "react";
 
 function App() {
+	const [youtubeID] = useState("jeKFPHa9Kec?si=fAHuxJi46pqYoX4x");
+
 	const test = async () => {
 		const chatCompletion = await openai.chat.completions.create({
-		messages: [{ role: 'user', content: 'Say this is a test' }],
-		model: 'gpt-3.5-turbo',
+			messages: [{ role: "user", content: "Say this is a test" }],
+			model: "gpt-3.5-turbo",
 		});
-	
+
 		alert(chatCompletion.choices);
-	}
-	
+	};
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<button onClick={test}>
-					Test OpenAI
-				</button>
-			</header>
-		</div>
+		<>
+			<button onClick={test}>Test OpenAI</button>
+			<div className="App">
+				<h1>Youtube embed: </h1>
+				<div className="youtube-container">
+					<iframe
+						className="video"
+						title="Youtube player"
+						sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
+						src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}
+					></iframe>
+				</div>
+			</div>
+		</>
 	);
 }
 
