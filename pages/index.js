97f7e4ebaea122d7721a2e1d5e1acc6dafce8789
kozr <zxzflow@gmail.com>
2023-10-15 02:13:20 -0700

    landing page css

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,15 @@ const Landing = () => {
         className="px-[5vw] h-full w-full flex flex-row items-center text-black dark:text-white bg-white dark:bg-black"
         id="main-content"
       >
-        <div className="flex flex-col mx-auto w-1/3">
-          <p>Synthesis</p>
-
+        <div className="flex flex-col mx-auto w-1/3 relative z-10 mt-[-100px]"> {/* <-- Added mt-[-20] here */}
+          <p className="my-2 drop-shadow-['0px 0px 22px rgba(123, 159, 174, 0.70)] text-[#F2F4FB] text-[128px] leading-[8rem] font-semibold font-['Syne']">synthesis</p>
+          <p className="my-2 bg-gradient-to-r from-[#D7FFF0] to-[#7BFFCF] font-['Syne'] bg-clip-text text-transparent text-[55px] font-semibold">where curiosity thrives.</p>
+          <p className="my-2 text-[#B0D5E1] text-[22px] mb-8">reimagining the video learning experience, empowering you to master any topic on the web.</p>
           <LandingLinkInput onChange={onChange} value={link} onSubmit={onSubmit} />
           </div>
+          <img src="/assets/bird.svg" className="absolute bottom-[30%] right-[20%] w-[10vw] h-[10vw] object-contain" alt="bird" />
       </div>
+      <img className="absolute bottom-0 z-0 w-full h-full" src="assets/landingBackground.png"></img>
     </div>
   );
 };
--- src/frontend/LandingLinkInput.jsx
+++ src/frontend/LandingLinkInput.jsx
@@ -1,20 +1,19 @@
 "use client";
 
 import React from "react";
-import Frame from "./Frame";
 
 const LinkInput = ({ value, onChange, onSubmit }) => {
   return (
       <form onSubmit={onSubmit}>
         <div className="flex w-full flex-row justify-between py-1">
           <input
-            className="w-11/12 dark:bg-grey p-4 rounded-md text-zinc-300 text-lg focus- placeholder-[#B0D5E1] outline-none"
+            className="w-11/12 dark:bg-grey p-4 rounded-lg text-zinc-300 text-lg focus- placeholder-[#B0D5E1] outline-none"
             type="text"
             value={value}
             onChange={onChange}
             placeholder="Insert a youtube video URL"
           />
-          <button className="ml-2 w-40 text-black text-lg bg-[#0DEFE1] rounded-md font-semibold bg-gradient-to-r from-[#0DEFE1] to-[#78FF96]" type="submit">
+          <button className="ml-5 w-40 text-black text-lg bg-[#0DEFE1] rounded-lg font-semibold bg-gradient-to-r from-[#0DEFE1] to-[#78FF96]" type="submit">
             Get Started
           </button>
         </div>
